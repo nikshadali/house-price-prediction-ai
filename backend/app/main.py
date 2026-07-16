15251f5d8e0d8d6f50d169a54fa86a3d6ea14dd0
nikshadali <nikali92100@gmail.com>
2026-07-16 13:51:26 +0500

    fix cors issue

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,21 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI(
     title="House Price Prediction API",
     description="Predict California house prices using a trained Machine Learning model.",
     version="1.0.0",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+        "https://house-price-prediction-ai-peach.vercel.app",
+        "https://house-price-prediction-ai-peach.vercel.app/"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 @app.get("/")
